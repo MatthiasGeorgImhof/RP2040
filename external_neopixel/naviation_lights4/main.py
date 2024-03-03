2a0a0ac92762f9bf5b105483cd04c498b7c41daa
Matthias Georg Imhof <mgimhof@yahoo.com>
2024-03-03 12:02:08 -0600

    refactor naviation_lights for better readability and switch behavior

--- external_neopixel/naviation_lights4/main.py
+++ external_neopixel/naviation_lights4/main.py
@@ -1,8 +1,12 @@
 import neopixel
 import time
 
-switch = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-is_running = True
+GPIO_OUT = (3,5,27,29)
+GPIO_IN = 14
+
+is_running = False
+switch_is_active = False
+switch_was_active = switch_is_active
 
 green = (0,255,0)
 red = (255,0,0)
@@ -21,34 +25,36 @@ colors0 = (wwgg, dddd, wwgg, dddd, wwgg, dddd, wwgg, dddd)
 colors1 = (wwrr, dddd, wwrr, dddd, wwrr, dddd, wwrr, dddd)
 colors2 = (oogg, dddd, oogg, dddd, oogg, dddd, oogg, dddd)
 colors3 = (oorr, dddd, oorr, dddd, oorr, dddd, oorr, dddd)
-delays =  (0.01,  0.19, 0.01, 0.19, 0.01, 0.19,  0.9,  0.1)
+delays =  (0.10, 0.10, 0.10, 0.10, 0.10, 0.10, 0.90, 0.10)
 
-pin_numbers = (3,5,27,29)
-hw_pins = tuple(machine.Pin(n, machine.Pin.OUT) for n in pin_numbers)
-neo_pixels = tuple(neopixel.NeoPixel(hw_pins[n],4) for n in range(4))
+switch = machine.Pin(GPIO_IN, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-#is rgb
+hw_pins = tuple(machine.Pin(n, machine.Pin.OUT) for n in GPIO_OUT)
+neo_pixels = tuple(neopixel.NeoPixel(hw_pins[n],4) for n in range(len(dddd)))
+
+def set_rgb(neo_pixel, color):
+    for i,rgb in enumerate(color):
+        neo_pixel[i] = rgb
+        neo_pixel.write()
 
 def set_all_dark():
           for neo_pixel in neo_pixels:
-              for i,rgb in enumerate(dddd):
-                  neo_pixel[i] = rgb
-                  neo_pixel.write()
+              set_rgb(neo_pixel, dddd)
 
 set_all_dark()
-is_running = False
 
 while(True):
   for delay, color0, color1, color2, color3 in zip(delays, colors0, colors1, colors2, colors3):
-      if switch.value():
-          print("switch", is_running)
+      switch_is_active = switch.value()
+      if switch_is_active and not switch_was_active:
           is_running = not is_running
+      
       if is_running:
           colors = (color0, color1, color2, color3)
           for neo_pixel, color in zip(neo_pixels, colors):
-              for i,rgb in enumerate(color):
-                  neo_pixel[i] = rgb
-                  neo_pixel.write()
+              set_rgb(neo_pixel, color)
       else:
           set_all_dark()
+    
       time.sleep(delay)
+      switch_was_active = switch_is_active
